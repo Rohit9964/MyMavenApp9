3f292d2a4faabea970003bdd078dbbbc3410f683
Rohit9964 <rohithonakhande2@gmail.com>
2025-04-03 11:23:29 +0530

    first

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int num1  = 10;
+        int num2  = 20;
+        int sum = 0;
+        System.out.println("num1 = " + num1 + "\t" + "num2 = " + num2 );
+        System.out.println("sum = " + (num1+num2));
     }
 }
